cf63f0c637bfb7b52940fcea15fc09655fa74ccb
Raphael-789 <c.raphaellius@outlook.com>
2025-11-06 22:45:36 +0800

    Add files via upload

---
+++ 欢迎入学.py
@@ -0,0 +1,8 @@
+a=input()
+print('|++++++++++++++++++++++|')
+print('|                      |')
+print('|   Welcome to WHUT    |')
+print('|                      |')
+print('|++++++++++++++++++++++|')
+print('欢迎您，',a,'同学！',sep='')
+#sep分隔符“seprator”默认是空格
